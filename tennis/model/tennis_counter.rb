4ea973d6a9f3f3d4c1a6f41a31ad8e0669bbbddc
adrian <juri.adrian@gmail.com>
2015-04-25 15:54:22 -0300

    refactor

--- tennis/model/tennis_counter.rb
+++ tennis/model/tennis_counter.rb
@@ -20,6 +20,18 @@ class TennisCounter
 		return @counterPlayerOne['points']
 	end
 
+	def counterPlayerTwo
+		return @counterPlayerTwo
+	end
+
+	def scoredPoint(player)
+		if player == 'playerOne'
+			playerOneScoredAPoint
+		else
+			playerTwoScoredAPoint
+		end
+	end
+
 	def playerOneScoredAPoint
 		case @counterPlayerOne['points']
 		when 0
@@ -33,12 +45,34 @@ class TennisCounter
 		end
 	end
 
+	def playerTwoScoredAPoint
+		case @counterPlayerTwo['points']
+		when 0
+			@counterPlayerTwo['points'] += 15
+		when 15
+			@counterPlayerTwo['points'] += 15
+		when 30
+			@counterPlayerTwo['points'] += 10
+		when 40
+			playerOneWinGame
+		end
+	end
+
 	def playerOneWinGame
 		if @counterPlayerOne['games'] == 5
 			playerOneWinSet
 		else
 			@counterPlayerOne['games'] += 1
 			@counterPlayerOne['points'] = 0
+		end
+	end
+
+	def playerTwoWinGame
+		if @counterPlayerTwo['games'] == 5
+			playerOneWinSet
+		else
+			@counterPlayerTwo['games'] += 1
+			@counterPlayerTwo['points'] = 0
 		end
 	end
 
--- tennis/spec/tennis_counter_spec.rb
+++ tennis/spec/tennis_counter_spec.rb
@@ -8,15 +8,21 @@ describe 'TennisCounter' do
 
 	tennisCounter = TennisCounter.new(playerOne, playerTwo)
 
-	it 'should return 0' do
+	it 'should return 0 when the match starts' do
 		tennisCounter.pointsPlayerOne.should be 0
 		tennisCounter.pointsPlayerTwo.should be 0
 	end
 
 	it 'should return 15 when playerOne scored the first point' do
-		tennisCounter.playerOneScoredAPoint
+		tennisCounter.scoredPoint(playerOne)
 		tennisCounter.counterPlayerOne['points'].should be 15
 		tennisCounter.counterPlayerOne['sets'].should be 0
+	end
+
+	it 'should return 15 15 when playerTwo also socored a point' do
+		tennisCounter.scoredPoint(playerTwo)
+		tennisCounter.counterPlayerOne['points'].should be 15
+		tennisCounter.counterPlayerTwo['points'].should be 15
 	end
 
 	it 'should return 0 points and 1 game when player one wins a game' do
